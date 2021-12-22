21a32d35444500e4eb4a95ef78de0f4f57865d9c
Fernnado <fcnogueirarp@gmail.com>
2021-12-22 17:01:10 -0300

    crud completo - backend concluido

--- React/AlunosApi/AlunosApi/Models/Aluno.cs
+++ React/AlunosApi/AlunosApi/Models/Aluno.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AlunosApi.Models
 {
+    [Table("Alunos")] //desnecessário acaba sendo redundante pois no db context já efetuei um map de aluno para alunos
     public class Aluno
     {
+        [Key] //desnecessário acaba sendo redundante pois através do id o entity já entende que temos uma chave primária
         public int Id { get; set; }
 
         [Required]
--- React/AlunosApi/AlunosApi/Startup.cs
+++ React/AlunosApi/AlunosApi/Startup.cs
@@ -1,7 +1,10 @@
+using AlunosApi.Context;
+using AlunosApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +29,13 @@ namespace AlunosApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAlunoService, AlunosService>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AlunosApi", Version = "v1" });
